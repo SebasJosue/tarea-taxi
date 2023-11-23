e9f542a25ca31dafb667bd096e238950cf729f15
Sebastián Paredes <146010608+SebasJosue@users.noreply.github.com>
2023-11-23 10:08:20 -0500

    Initial commit

---
+++ tarea-taxi.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let taxi = {
+    craetion: '2014',
+    life:  10,
+}
+
+taxi.creation = true;
+
+alert(taxi.creation +' '+ taxi.life + ' taxi ' + taxi.creation);
+
+delete taxi.life
